71b48120d1e4e9f421e1ded26136d48cc47faf9b
okothbrain1 <okothbrain1@gmail.com>
2020-07-17 04:24:46 -1200

    The search bar functionality

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,6 +4,8 @@ import { BehaviorSubject } from 'rxjs';
 import { CartService } from '../services/cart.service';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { firestore } from 'firebase';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tab1',
@@ -15,19 +17,43 @@ export class Tab1Page {
   cart = [];
   products = [];
   cartItemCount: BehaviorSubject<number>;
+
+ //Declaring an array to hold the product items
+ public itemList: any[];
+
  
   @ViewChild('cart', {static: false, read: ElementRef})fab: ElementRef;
  
-  constructor(private cartService: CartService, private modalCtrl: ModalController,firestore: AngularFirestore) {
+  constructor(private cartService: CartService, private modalCtrl: ModalController,private firestore: AngularFirestore) {
     
   }
  
-  ngOnInit() {
-    this.items = this.cartService.retrieve_products();
+  async ngOnInit() {
+    //this.items = this.cartService.retrieve_products();
     this.cart = this.cartService.getCart();
     this.cartItemCount = this.cartService.getCartItemCount();
+    this.itemList = await this.initializeItems();
   }
- 
+//function to fetch the items when a query is to be performed
+  async initializeItems(): Promise <any>{
+    const itemList = await this.firestore.collection('items').valueChanges().pipe(first()).toPromise();
+    return itemList;
+    
+  }
+//function to filter the items when the user starts the search task
+async filterList(evt){
+  this.itemList = await this.initializeItems();
+  const searchTerm = evt.srcElement.value;
+  if(!searchTerm){
+    return;
+  }
+  this.itemList = this.itemList.filter(currentItem => {
+    if (currentItem.ProductName && searchTerm) {
+      return (currentItem.ProductName.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1);
+    }
+  });
+} 
+
   addToCart(product) {
     this.cartService.addProduct(product);
   
